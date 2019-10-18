96cd9bdf76ca4b6e169e67746fa6e3acdb14b7d8
Chidubem Ogbudibe <ogbudibe@mail.utoronto.ca>
2019-10-18 01:20:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(x)
   if(x % 3 == 0)
     print "Fizz"
-  elsif(x % 5 == 0)
+  end
+  if(x % 5 == 0)
     print "Buzz"
-  elsif(x % 3 == 0 && x % 5 == 0)
-    print "FizzBuzz"
   end
 end
 
